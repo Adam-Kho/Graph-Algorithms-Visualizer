1dc33c4c116408351715a77500641dd859fe6b6c
Adam <akhoshnaw100@gmail.com>
2025-12-23 21:51:17 -0800

    Client Menu

--- graph.h
+++ graph.h
@@ -1,1 +1,18 @@
 // Adam Khoshnaw
+
+
+class Graph {
+
+    public:
+        Graph() { }
+
+        bool addVertex(int vertex) { }
+
+        bool addEdge(int v1, int v2) { }
+
+        bool displayGraph() { }
+
+    private:
+        std::map<int, std::vector<int>> theGraph;
+
+}
--- main.cpp
+++ main.cpp
@@ -2,6 +2,51 @@
 
 int main() {
 
+    Graph objGraph;
+    int choice = 0;
+    int vertexNum = 0;
+    int vertexOne = 0;
+    int vertexTwo = 0;
+
+    std::cout << "Graph Algorithms Visualizer!\n" << std::endl;
+
+    // menu interface
+    do {
+        // CHANGE TO FUNCTION FOR CLEANER MAIN
+        std::cout << "0) Exit program\n";
+        std::cout << "1) Add vertex\n";
+        std::cout << "2) Add edge\n";
+        std::cout << "3) Display Graph\n";
+
+        std::cout << "Enter choice: ";
+        std::cin >> choice;
+        std::cout >> "\n";
+
+        switch(choice) {
+            case (1):
+                std::cout << "Integer value: ";
+                std::cin >> vertexNum;
+                std::cout << "\n";
+                objGraph.addVertex(vertexNum);
+
+                break;
+            case (2):
+                std::cout << "Vertex 1: ";
+                std::cin >> vertexOne;
+                std::cout << "\n";
+                std::cout << "Vertex 2: ";
+                std::cin >> vertexTwo;
+                std::cout << "\n";
+
+                objGraph.addEdge(vertexOne, vertexTwo);
+
+                break;
+            case (3):
+                objGraph.displayGraph();
+
+                break;
+        }
+    } while (choice != 0);
 
 
     return 0;
